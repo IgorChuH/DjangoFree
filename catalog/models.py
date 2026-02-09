6f131364b7d75840f72a5ae8291605c518f6a469
Чухутин Игорь <chukhutinigor@yandex.ru>
2026-02-09 19:57:14 +0300

    Описание

--- catalog/models.py
+++ catalog/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Category(models.Model):
     category_name = models.CharField(max_length=150, verbose_name="Категория")
-    description = models.TextField(null=True)
+    description = models.TextField(null=True, verbose_name="Описание")
 
     def __str__(self):
         return self.category_name
